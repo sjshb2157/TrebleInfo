4bb64c68752528f9f7d20e0bedfa5eed40c063ef
Hackintosh 5 <git@hack5.dev>
2023-01-28 11:20:08 +0000

    Move details placeholder to center

--- app/src/main/java/tk/hack5/treblecheck/ui/screens/Details.kt
+++ app/src/main/java/tk/hack5/treblecheck/ui/screens/Details.kt
@@ -123,23 +123,35 @@ fun DetailsList(
             Spacer(Modifier.height(innerPadding.calculateBottomPadding()))
         }
         if (twoColumn) {
-            Column(
-                Modifier
-                    .verticalScroll(rememberScrollState())
-                    .weight(0.5f)
-                    .consumeWindowInsets(innerPadding.vertical())
-            ) {
-                Spacer(Modifier.height(innerPadding.calculateTopPadding()))
-                val modifier = Modifier.safeDrawingPadding().padding(start = gutter / 2, end = pageHorizontalPadding).fillMaxWidth()
-                openDialog?.let { index ->
+            openDialog?.let { index ->
+                Column(
+                    Modifier
+                        .verticalScroll(rememberScrollState())
+                        .weight(0.5f)
+                        .consumeWindowInsets(innerPadding.vertical())
+                ) {
+                    Spacer(Modifier.height(innerPadding.calculateTopPadding()))
+                    val modifier = Modifier.safeDrawingPadding()
+                        .padding(start = gutter / 2, end = pageHorizontalPadding).fillMaxWidth()
                     val detail = details[index]
                     Text(detail.title, modifier, style = MaterialTheme.typography.titleLarge)
                     Text(detail.subtitle, modifier, style = MaterialTheme.typography.titleMedium)
                     Text(detail.body, modifier, style = MaterialTheme.typography.bodyMedium)
-                } ?: run {
+                    Spacer(Modifier.height(innerPadding.calculateBottomPadding()))
+                }
+            } ?: run {
+                Column(
+                    Modifier
+                        .weight(0.5f)
+                        .padding(innerPadding)
+                        .fillMaxHeight()
+                        .consumeWindowInsets(innerPadding.vertical()),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    val modifier = Modifier.safeDrawingPadding()
+                        .padding(start = gutter / 2, end = pageHorizontalPadding).fillMaxWidth()
                     Text(stringResource(R.string.detail_placeholder), modifier)
                 }
-                Spacer(Modifier.height(innerPadding.calculateBottomPadding()))
             }
         }
     }
