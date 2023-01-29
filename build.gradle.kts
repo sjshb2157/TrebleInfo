12c9edfea06c91af533e2303eafb05ed48c6c341
Hackintosh 5 <git@hack5.dev>
2023-01-29 12:28:53 +0000

    Update libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        //classpath(project(":poeditor-android"))
-        //classpath(project(":materialdesignicons-android"))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
